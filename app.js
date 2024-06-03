f99a1034dd5306bda98cc9bfa1e9ece88b02159f
Viyati <36640171+sundarlal12@users.noreply.github.com>
2024-06-03 17:31:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get('/', async (req, res) => {
     res.send(pdfBuffer);
   } catch (error) {
     console.error('Error generating PDF:', error);
-    res.status(500).send('Error generating PDF');
+    res.status(500).send('Error generating PDF!!!');
   }
 });
 
